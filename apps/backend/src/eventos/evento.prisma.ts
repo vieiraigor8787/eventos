80efa3d57ec11ec369f8543f1a5c035d16a9857a
IgorCerq22 <igor.cerqueira@jussi.com.br>
2024-12-07 11:06:24 -0300

    funções buscar eventos, ajuste db, prisma provider

---
+++ apps/backend/src/eventos/evento.prisma.ts
@@ -0,0 +1,61 @@
+import { Convidado, Evento } from 'core';
+import { Injectable } from '@nestjs/common';
+import { PrismaProvider } from 'src/db/prisma.provider';
+
+@Injectable()
+export class EventoPrisma {
+  constructor(readonly prisma: PrismaProvider) {}
+
+  salvar(evento: Evento) {
+    return this.prisma.evento.create({
+      data: evento as any,
+    });
+  }
+
+  salvarConvidado(evento: Evento, convidado: Convidado) {
+    return this.prisma.convidado.create({
+      data: {
+        ...convidado,
+        qtdeAcompanhantes: +(convidado.qtdeAcompanhantes ?? 0),
+        evento: {
+          connect: { id: evento.id },
+        },
+      },
+    });
+  }
+
+  async buscarTodos(): Promise<Evento[]> {
+    return this.prisma.evento.findMany() as any;
+  }
+
+  async buscarPorId(
+    id: string,
+    completo: boolean = false,
+  ): Promise<Evento | null> {
+    return this.prisma.evento.findUnique({
+      where: { id },
+      include: { convidados: completo },
+    }) as any;
+  }
+
+  async buscarPorAlias(
+    alias: string,
+    completo: boolean = false,
+  ): Promise<Evento | null> {
+    return this.prisma.evento.findUnique({
+      select: {
+        id: true,
+        alias: true,
+        nome: true,
+        descricao: true,
+        data: true,
+        local: true,
+        publicoEsperado: completo,
+        imagem: true,
+        imagemBackground: true,
+        convidados: completo,
+      },
+      where: { alias },
+    }) as any;
+  }
+}
--- apps/backend/src/eventos/eventos.module.ts
+++ apps/backend/src/eventos/eventos.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { DbModule } from 'src/db/db.module';
+import { EventosController } from './eventos.controller';
 
-@Module({})
+@Module({
+  imports: [DbModule],
+  controllers: [EventosController],
+})
 export class EventosModule {}
